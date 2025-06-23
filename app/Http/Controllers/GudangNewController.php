0dfa5c439296df06ad94284ab3491e372511c06e
nadws <nandw567@gmail.com>
2025-06-23 11:02:37 +0800

    update GudangNewController.php

--- app/Http/Controllers/GudangNewController.php
+++ app/Http/Controllers/GudangNewController.php
@@ -133,7 +133,7 @@ class GudangNewController extends Controller
                     'grade_id' => $id,
                     'rwb_id' => $r->rwb[$x],
                     'nm_partai' => $r->ket2[$x],
-                    'no_invoice' => date('d', strtotime($r->tgl[$x])) . '-' . date('mY', strtotime($r->tgl[$x])) . '-' . $kode_brg .  $kode_rwb . '-' . date('m', strtotime($r->tgl[$x] . ' +6 months')) . '-' . date('Y', strtotime($r->tgl[$x] . ' +6 months')),
+                    'no_invoice' => date('d', strtotime($r->tgl[$x])) . '-' . date('mY', strtotime($r->tgl[$x])) . '-' . $kode_brg .  $kode_rwb . '-' . date('m', strtotime($r->tgl[$x] . ' +6 months')) . '-' . date('y', strtotime($r->tgl[$x] . ' +6 months')),
                     'pcs' => $r->pcs[$x],
                     'kg' => $r->gr[$x] / 1000,
                     'no_kendaraan' => $r->no_kendaraan[$x],
