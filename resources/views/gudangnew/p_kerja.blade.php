26db0c2a1e39a71e73e3222931f7e1d05ca669a5
nadws <nandw567@gmail.com>
2025-11-14 10:35:32 +0800

    update p_kerja.blade.php

--- resources/views/gudangnew/p_kerja.blade.php
+++ resources/views/gudangnew/p_kerja.blade.php
@@ -30,7 +30,9 @@
                     <table class="float-end">
                         <td>Search :</td>
                         <td><input type="text" id="pencarian" class="form-control float-end"> </td>
-                        <td><button class="btn btn-primary float-end ms-2">Simpan</button></td>
+                        <td>
+                            {{-- <button class="btn btn-primary float-end ms-2">Simpan</button> --}}
+                        </td>
                         <td>
                             <a href="{{ route('gudangnew.export_p_kerja') }}"
                                 class="btn btn-success float-end ms-2 me-2"><i class="fas fa-file-excel"></i> Opname</a>
@@ -55,7 +57,7 @@
                                 <th class="text-end dhead">Gr Susut</th>
                                 <th class="text-end text-white bg-danger">Pcs Sisa</th>
                                 <th class="text-end text-white bg-danger">Gr Sisa</th>
-                                <th class="text-end dhead" width="200px">Keterangan Opname</th>
+                                {{-- <th class="text-end dhead" width="200px">Keterangan Opname</th> --}}
                             </tr>
                         </thead>
                         <tbody>
@@ -96,11 +98,11 @@
                                     </td>
                                     <td class="text-end">{{ number_format($WipSisaPcs, 0) }}</td>
                                     <td class="text-end">{{ number_format($WipSisaGr, 0) }}</td>
-                                    <td>
+                                    {{-- <td>
                                         <input type="hidden" name="partai[]" value="{{ $g->ket2 }}">
                                         <input type="text" class="form-control" name="ket[]"
                                             value="{{ $g->opname_bulan }}">
-                                    </td>
+                                    </td> --}}
 
                                 </tr>
                             @endforeach
