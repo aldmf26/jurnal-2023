d58dd0daf19b7aeed93ef4c9167f2efa9b7b3fc9
Fahrizaldi <45739897+aldmf26@users.noreply.github.com>
2026-08-10 15:17:25 +0800

    fix: include VPTH grams in congan detail export

--- app/Http/Controllers/ConganController.php
+++ app/Http/Controllers/ConganController.php
@@ -838,10 +838,12 @@ class ConganController extends Controller
         $sheet1->setCellValue('K7', 'Harga' . 100 . '%');
         $sheet1->setCellValue('K8', 'Harga FIx');
 
-        $sheet1->setCellValue('L4', $ttl_gr);
+        $totalGram = $ttl_gr + $ttl_gr2;
+
+        $sheet1->setCellValue('L4', $totalGram);
         $sheet1->setCellValue('L5', $invoice->hrga_beli);
-        $sheet1->setCellValue('L6', $ttl_gr > 0 ? round((($total_rp + $total_rp2) / ($ttl_gr + $ttl_gr2)) * ((100 - $invoice->persen_air) / 100), 0) : 0);
-        $sheet1->setCellValue('L7', $ttl_gr > 0 ? round(($total_rp + $total_rp2) / ($ttl_gr + $ttl_gr2), 0) : 0);
+        $sheet1->setCellValue('L6', $totalGram > 0 ? round((($total_rp + $total_rp2) / $totalGram) * ((100 - $invoice->persen_air) / 100), 0) : 0);
+        $sheet1->setCellValue('L7', $totalGram > 0 ? round(($total_rp + $total_rp2) / $totalGram, 0) : 0);
         $sheet1->setCellValue('L8', $invoice->selesai);
 
 
