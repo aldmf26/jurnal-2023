5df6891e9e286b3283843a39179863e9cd13641e
nadws <nandw567@gmail.com>
2026-08-21 10:21:17 +0800

    update ConganController.php and resources/views/pembelian_bk/index.blade.php

--- app/Http/Controllers/ConganController.php
+++ app/Http/Controllers/ConganController.php
@@ -831,7 +831,7 @@ class ConganController extends Controller
         $sheet1->setCellValue('K7', 'Harga' . 100 . '%');
         $sheet1->setCellValue('K8', 'Harga FIx');
 
-        $sheet1->setCellValue('L4', $ttl_gr);
+        $sheet1->setCellValue('L4', $ttl_gr + $ttl_gr2);
         $sheet1->setCellValue('L5', $invoice->hrga_beli);
         $sheet1->setCellValue('L6', $ttl_gr > 0 ? round((($total_rp + $total_rp2) / ($ttl_gr + $ttl_gr2)) * ((100 - $invoice->persen_air) / 100), 0) : 0);
         $sheet1->setCellValue('L7', $ttl_gr > 0 ? round(($total_rp + $total_rp2) / ($ttl_gr + $ttl_gr2), 0) : 0);
